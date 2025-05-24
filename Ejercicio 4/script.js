f4459e3afea405377863f5a9bdeaadd387257203
Díaz Marcelo Gabriel <gabrielmarcelodz@gmail.com>
2025-05-23 22:00:56 -0300

    Agrego ejercicio 4

---
+++ Ejercicio 4/script.js
@@ -0,0 +1,10 @@
+function calcularIMC() {
+  let peso = parseFloat(document.getElementById("peso").value);
+  let altura = parseFloat(document.getElementById("altura").value);
+  if (peso > 0 && altura > 0) {
+    let imc = peso / (altura * altura);
+    alert("Tu IMC es: " + imc.toFixed(2));
+  } else {
+    alert("Ingrese valores válidos.");
+  }
+}
